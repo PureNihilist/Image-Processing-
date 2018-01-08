c4ed8c86fb06be9e263770863b1e13c36aa3e5a3
Mateusz Galas <s0ulhunter@wp.pl>
2018-01-08 13:08:04 +0100

    Image processing and binarization

--- ImageProcessing/src/imageprocessing/ImageProcessing.java
+++ ImageProcessing/src/imageprocessing/ImageProcessing.java
@@ -1,7 +1,9 @@
 package imageprocessing;
 
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
@@ -14,41 +16,76 @@ public class ImageProcessing  extends Component {
   public static void main(String[] foo) {
     new ImageProcessing();
   }
-
-  public void printPixelARGB(int pixel) {
-    int alpha = (pixel >> 24) & 0xff;
-    int red = (pixel >> 16) & 0xff;
-    int green = (pixel >> 8) & 0xff;
-    int blue = (pixel) & 0xff;
-    if(red != 255 || green != 255|| blue != 255) {
-        System.out.println("argb: " + alpha + ", " + red + ", " + green + ", " + blue);
-    }
+  public static void identifyShape() {
+  
+      
   }
+  
+  
+    public static double max(double[] a) { // Compute maximum value in a[]
+        double max = Double.NEGATIVE_INFINITY;
+        for (int i = 0; i < a.length; i++)
+            if(a[i] > max)
+                max = a[i];
+        return max;
+    }
 
-  private void marchThroughImage(BufferedImage image) {
-    int w = image.getWidth();
-    int h = image.getHeight();
-    System.out.println("width, height: " + w + ", " + h);
+    public static double mean(double[] a) { // Compute the average of the values in a[]
+        double sum = 0.0;
+        for (int i = 0; i < a.length; i++)
+            sum += a[i];
+        return sum / a.length;
+    }
 
-    for (int i = 0; i < h; i++) {
-      for (int j = 0; j < w; j++) {
-     //   System.out.println("x,y: " + j + ", " + i);
-        int pixel = image.getRGB(j, i);
-        printPixelARGB(pixel);
-     //   System.out.println("");
+    private void marchThroughImage(BufferedImage image) {
+      int w = image.getWidth();
+      int h = image.getHeight();
+      
+    //  System.out.println("width, height: " + w + ", " + h);
+      double [][] pixels = new double[w][h];
+      Color myWhite = new Color(255, 255, 255); // Color white
+      Color myBlack = new Color(0,0,0); // black
+      int white = myWhite.getRGB();
+      int black = myBlack.getRGB();
+      for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+       //   System.out.println("x,y: " + j + ", " + i);
+          int pixel = image.getRGB(j, i);
+          int alpha = (pixel>>24)&0xff;
+          int red = (pixel >> 16) & 0xff;
+          int green = (pixel >> 8) & 0xff;
+          int blue = (pixel) & 0xff;
+          int avg = (red+green+blue)/3;
+      //    pixel = (alpha<<24) | (avg<<16) | (avg<<8) | avg;
+          if(red <= 50 && green <= 50 && blue <= 50) {
+          //System.out.println(red + ", " + green + ", " + blue);
+            pixels[j][i] = 1;
+            image.setRGB(j, i, black);
+          } else {
+            pixels[j][i] = 0;
+            image.setRGB(j, i, white);
+          }
+        }
+      }
+      
+      
+      try {
+          File outputfile = new File("output_image.jpg");
+          ImageIO.write(image, "jpg", outputfile);
+      } catch(IOException ex) {
+          ex.printStackTrace();
       }
     }
-  }
 
-  public ImageProcessing() {
-    try {
-      // get the BufferedImage, using the ImageIO class
-      BufferedImage image = 
-        ImageIO.read(this.getClass().getResource("img.jpg"));
-      marchThroughImage(image);
-    } catch (IOException e) {
-      System.err.println(e.getMessage());
+    public ImageProcessing() {
+      try {
+        // get the BufferedImage, using the ImageIO class
+        BufferedImage image = 
+          ImageIO.read(this.getClass().getResource("image.jpg"));
+        marchThroughImage(image);
+      } catch (IOException e) {
+        System.err.println(e.getMessage());
+      }
     }
-  }
 }
 
