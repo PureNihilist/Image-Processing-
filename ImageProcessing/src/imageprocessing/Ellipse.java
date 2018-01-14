85f4f9621b75e4e64b81f2ca64bf432e34fa73e7
Mokasyn56 <hubert_b@op.pl>
2018-01-14 19:59:41 +0100

    Refactored Ellipse Class

--- ImageProcessing/src/imageprocessing/Ellipse.java
+++ ImageProcessing/src/imageprocessing/Ellipse.java
@@ -1,13 +1,34 @@
 package imageprocessing;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Mateusz Galas
  */
 public class Ellipse extends Shape{
-   /*
-    public Ellipse(int width, int height) {
-        super(width, height);
+
+    Pixel Center;
+    ArrayList<Pixel> ArraysOfPixels;
+    
+    public Ellipse(Pixel Center, ArrayList<Pixel> pixarray) {
+       this.ArraysOfPixels = new ArrayList<>(pixarray);
+       this.Center = Center;
     }
-   */
+    
+    public Pixel getCenter(){
+        return this.Center;
+    }
+
+    public ArrayList<Pixel> getArraysOfPixels() {
+        return this.ArraysOfPixels;
+    }
+
+    public void setCenter(Pixel Center){
+        this.Center = Center;
+    }
+    
+    public void setArraysOfPixels(ArrayList<Pixel> ArraysOfPixels) {
+        this.ArraysOfPixels = ArraysOfPixels;
+    }
 }
