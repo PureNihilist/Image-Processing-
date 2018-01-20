17a65bbdeac785065342020cf60f33d1b4874060
Mateusz Galas <s0ulhunter@wp.pl>
2018-01-20 18:12:08 +0100

    Ellipse recognition

--- ImageProcessing/src/imageprocessing/Ellipse.java
+++ ImageProcessing/src/imageprocessing/Ellipse.java
@@ -10,10 +10,50 @@ public class Ellipse extends Shape{
 
     private Pixel Center;
     private ArrayList<Pixel> ArraysOfPixels;
-    
-    public Ellipse(Pixel Center, ArrayList<Pixel> pixarray) {
+    private Pixel left;
+    private Pixel right;
+    private Pixel up;
+    private Pixel down;
+       
+    public Ellipse(Pixel left, Pixel right, Pixel up, Pixel down, Pixel Center, ArrayList<Pixel> pixarray) {
+       this.left = left;
+       this.right = right;
+       this.up = up;
+       this.down = down;
        this.ArraysOfPixels = new ArrayList<>(pixarray);
        this.Center = Center;
+    }
+    
+    Pixel getLeft(){
+        return this.left;
+    }
+    
+    Pixel getRight(){
+        return this.right;
+    }
+    
+    Pixel getUp(){
+        return this.up;
+    }
+    
+    Pixel getDown(){
+        return this.down;
+    }
+    
+    void setUp (Pixel arg) {
+        this.up = arg;
+    }
+    
+    void setLeft (Pixel arg) {
+        this.left = arg;
+    }
+    
+    void setRight (Pixel arg) {
+        this.right = arg;
+    }
+    
+    void setDown (Pixel arg) {
+        this.down = arg;
     }
     
     public Pixel getCenter(){
--- ImageProcessing/src/imageprocessing/ImageProcessing.java
+++ ImageProcessing/src/imageprocessing/ImageProcessing.java
@@ -7,8 +7,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import javax.imageio.ImageIO;
-import java.util.Vector;
-
 /**
  *
  * @author Mateusz Galas
@@ -17,11 +15,9 @@ import java.util.Vector;
 public class ImageProcessing  extends Component {
     
     static double [][] pixels;
-    
     ArrayList<Rectangle> rectangles = new ArrayList<>();
-    
+    ArrayList<Ellipse> ellipses = new ArrayList<>();
     BufferedImage output;
-    
     ArrayList<Pixel> corners = new ArrayList<>();
     ArrayList<Pixel> biases = new ArrayList<>();
     
@@ -30,8 +26,6 @@ public class ImageProcessing  extends Component {
     }
     /* detect corners */
     public void identifyShape() {
-       // Color myRed = new Color(255, 0, 0); // red
-       // int red = myRed.getRGB();
        for(int j = 0 ; j < pixels.length ; j++){
         for(int i = 0 ; i < pixels[j].length ; i++) {
                 if(pixels[j][i] == 1 ) {
@@ -79,15 +73,14 @@ public class ImageProcessing  extends Component {
                        /* lewy gorny do prawy dolny*/
                     } else if (i+1 < pixels[j].length && j+1 < pixels.length && pixels[j+1][i+1] == 1 ) {
                        if(!corners.contains(new Pixel(j,i+1))) {
-                        Pixel p1 = new Pixel(j, i);
-                        biases.add(p1);
-                        Pixel p2 = new Pixel(j+1, i+1);
-                        biases.add(p2);
-                       //  System.out.println(j + " " + i);
-                       // System.out.println(new Integer(j+1) + " " + new Integer(i+1));
+                            Pixel p1 = new Pixel(j, i);
+                            biases.add(p1);
+                            Pixel p2 = new Pixel(j+1, i+1);
+                            biases.add(p2);
+                        //System.out.println(j + " " + i);
+                        //System.out.println(new Integer(j+1) + " " + new Integer(i+1));
                        }
-                
-                    }
+                    } 
                 }
            }
        }
@@ -95,7 +88,6 @@ public class ImageProcessing  extends Component {
         while(true){
             processing = false;
             Pixel [] recPixels = new Pixel[4];
-            
             for (Pixel leftTop : corners) {
                 if(leftTop.getRole().equals("leftTop")){    
                     for (Pixel leftDown : corners) {
@@ -129,21 +121,107 @@ public class ImageProcessing  extends Component {
             if(!processing)
                 break;
         }
-        System.out.println("Rozpoznano: " + rectangles.size() + " prostokątów");
-        Color myBlack = new Color(0,0,0); // black
-        int black = myBlack.getRGB();        
+        System.out.println("Rozpoznano: " + rectangles.size() + " prostokąty");        
         for(Rectangle r : rectangles) {
-            cleanBiasesFromCorners(r.getLeftTop());
-            cleanBiasesFromCorners(r.getRightTop());
-            cleanBiasesFromCorners(r.getLeftDown());
-            cleanBiasesFromCorners(r.getRightDown());
+           cleanBiasesFromCorners(r.getLeftTop());
+           cleanBiasesFromCorners(r.getRightTop());
+           cleanBiasesFromCorners(r.getLeftDown());
+           cleanBiasesFromCorners(r.getRightDown());
         }
+            
+        while(true) {
+            collectEllipses();
+            if(biases.isEmpty())
+                break;
+        }  
+        System.out.println("Rozpoznano: " + ellipses.size() + " ellipsy");
+        Color myBlack = new Color(0,0,0); // black
+        int black = myBlack.getRGB();
         for(Pixel p : biases) {
-            output.setRGB(p.getX(), p.getY(), black);
+            //System.out.println(p.getX() +  " " + p.getY());
+            output.setRGB(p.getX(), p.getY(), black);        
         }
-        
     }
     
+    void collectEllipses(){
+        int leftMostX = Integer.MAX_VALUE;
+        int leftMostY = 0;  
+        for(int i = 0 ; i < biases.size() ; i++){
+            Pixel p = biases.get(i);
+            if(p.getX() < leftMostX){
+                leftMostX = p.getX(); 
+                leftMostY = p.getY();
+            }
+        }    
+        Pixel leftMost = new Pixel(leftMostX, leftMostY);
+        int leftMostOppositeX = 0;
+        int leftMostUpperY = 0;
+        for(int i = 0 ; i < biases.size() ; i++) {
+            Pixel p = biases.get(i);
+            if(p.getX() == leftMost.getX() && p.getY() == leftMost.getY()) {
+                biases.remove(p);
+                continue;
+            }          
+            if(p.getX() == leftMost.getX()){
+                leftMostUpperY = p.getY();
+            }
+            if(p.getY() == leftMost.getY()){
+                leftMostOppositeX = p.getX();
+                break;
+            }
+        }
+        /*wypelnianie lewej strony, mozna zrobic dla innych stron*/
+        for(int j = leftMostUpperY ; j < leftMost.getY() ; j++){
+            Pixel p = new Pixel(leftMost.getX(), j);
+            if(pixels[leftMost.getX()][j] == 1 && !biases.contains(p)) 
+                biases.add(p);
+        }
+        int centerX = (leftMost.getX() - leftMostOppositeX)/2;
+        int centerY = (leftMost.getY() - leftMostUpperY)/2;
+        Pixel ellipseCenter = new Pixel(leftMostOppositeX + centerX,leftMostUpperY + centerY);
+        int leftMostUpY = 0;
+        for(int j = ellipseCenter.getY() ; j > 0 ; j--){
+            Pixel p = new Pixel(ellipseCenter.getX(), j);
+            if(pixels[ellipseCenter.getX()][j] == 1 && !biases.contains(p)) {
+                biases.add(p);
+                leftMostUpY = p.getY();
+                break;
+            }
+        }
+        int leftMostDownY = 0;
+        for(int j = ellipseCenter.getY() ; j < pixels.length ; j++){
+            Pixel p = new Pixel(ellipseCenter.getX(), j);
+            if(pixels[ellipseCenter.getX()][j] == 1 && !biases.contains(p)) {
+                biases.add(p);
+                leftMostDownY = p.getY();
+                break;
+            }
+        }
+        Pixel left = new Pixel(leftMost.getX(), ellipseCenter.getY());
+        Pixel right = new Pixel(leftMostOppositeX, ellipseCenter.getY());
+        Pixel up = new Pixel(ellipseCenter.getX(), leftMostUpY );    
+        Pixel down = new Pixel(ellipseCenter.getX(), leftMostDownY);
+      //  left.print(); right.print(); up.print(); down.print();
+      //  System.out.println("center"); ellipseCenter.print();
+        ArrayList<Pixel> ellipsePoints = new ArrayList<>();
+        for(Pixel p : biases) {
+            if(p.getX() >= leftMost.getX() && p.getX() <= leftMostOppositeX && p.getY() >= up.getY() && p.getY() <= down.getY()) 
+                ellipsePoints.add(p);
+        }
+        for(Pixel p : ellipsePoints){
+            if(biases.contains(p))
+                biases.remove(p);
+        }
+        Ellipse ellipse = new Ellipse(left,right,up,down,ellipseCenter,ellipsePoints);
+     //   System.out.println("rozpoznano ellipsę");
+        ellipses.add(ellipse);
+        /*
+        for(Pixel p : ellipsePoints){
+         output.setRGB(p.getX(), p.getY(), black);     
+        }*/
+    }
+    
+
     void cleanBiasesFromCorners(Pixel corner){
         corner.setY(corner.getY()+1); /*below*/
         biases.remove(searchForPixel(corner, biases));
@@ -154,9 +232,7 @@ public class ImageProcessing  extends Component {
         biases.remove(searchForPixel(corner, biases));
         corner.setX(corner.getX()+1);
         corner.setY(corner.getY()-1); /*upper*/
-        if(biases.remove(searchForPixel(corner, biases))){
-            System.out.println("4");
-        }
+        biases.remove(searchForPixel(corner, biases));       
     }
     
     Pixel searchForPixel(Pixel goal, ArrayList<Pixel> pixel_list){
@@ -184,7 +260,7 @@ public class ImageProcessing  extends Component {
           int red = (pixel >> 16) & 0xff;
           int green = (pixel >> 8) & 0xff;
           int blue = (pixel) & 0xff;
-          if(red <= 170 && green <= 170 && blue <= 170) {
+          if(red <= 50 && green <= 50 && blue <= 50) {
             pixels[j][i] = 1;
             output.setRGB(j, i, white);
           } else {
@@ -196,26 +272,24 @@ public class ImageProcessing  extends Component {
     }
     
     void drawOutputImage(BufferedImage image){
-      try {
-          File outputfile = new File("output_image.jpg");
-          ImageIO.write(image, "jpg", outputfile);
-      } catch(IOException ex) {
-          ex.printStackTrace();
-      }
+        try {
+            File outputfile = new File("output_image.jpg");
+            ImageIO.write(image, "jpg", outputfile);
+        } catch(IOException ex) {
+            ex.printStackTrace();
+        }
     }
     
-    
-
     public ImageProcessing() {
-      try {
-        BufferedImage image = ImageIO.read(this.getClass().getResource("img_two.jpg"));
-        output = image;
-        binarization(image);
-        identifyShape();
-        drawOutputImage(output); 
-      } catch (IOException e) {
-        System.err.println(e.getMessage());
-      }
+        try {
+            BufferedImage image = ImageIO.read(this.getClass().getResource("img_two.jpg"));
+            output = image;
+            binarization(image);
+            identifyShape();
+            drawOutputImage(output); 
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
 
